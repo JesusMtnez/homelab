a711438bb227b351fbbd8dd3a1ab99b4150fa9b7
JesusMtnez <jesusmartinez93@gmail.com>
2021-06-16 13:39:31 +0200

    Init project with nix setup

---
+++ shell.nix
@@ -0,0 +1,27 @@
+let
+
+  sources = import ./nix/sources.nix;
+  nixpkgs = sources.nixpkgs;
+  pkgs = import nixpkgs {};
+
+  k8s.version = "v1.21.1";
+
+in pkgs.mkShell rec {
+  name = "k3s-shell";
+
+  buildInputs = with pkgs; [
+    niv
+
+    # Tools
+    kubectl # TODO Use k8s.version
+    ansible
+    ansible-lint
+
+    # Local dev
+    (kube3d.override { k3sVersion = k8s.version + "+k3s1"; })
+  ];
+
+  shellHook = ''
+    export NIX_PATH="nixpkgs=${nixpkgs}
+  '';
+}
