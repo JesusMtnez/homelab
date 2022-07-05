b3f17c0a17d1c4b301cc75966cace5bf23312df5
JesusMtnez <jesusmartinez93@gmail.com>
2022-07-05 10:53:16 +0200

    move nixpkgs to release 22.05 branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "homelab";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    nixpkgs.url = github:NixOS/nixpkgs/release-22.05;
     flake-utils.url = "github:numtide/flake-utils";
   };
 
