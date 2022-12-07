aaa6907d2c8411e71ba43c8fc829a6623742f7ea
JesusMtnez <jesusmartinez93@gmail.com>
2022-12-07 19:39:12 +0100

    upgrade ansible-core to 2.14.0

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "homelab";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/release-22.05;
+    nixpkgs.url = github:NixOS/nixpkgs/release-22.11;
     flake-utils.url = "github:numtide/flake-utils";
   };
 
