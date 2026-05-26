40b33f44135484516c6256aef76f4d6e38ce62c1
JesusMtnez <jesusmartinez93@gmail.com>
2026-05-26 15:55:58 +0200

    chore(deps): update minerva to NixOS 26.05 "Yarara"

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "JesusMtnez's Homelab based on Nix!";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     nixpkgs-latest.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
